7dfd30b9c77e08818a11e5981b1bc6f9255f240b
Max Ruman <max@mvsk.io>
2025-11-09 15:44:40 +0800

    fix lsp for nvim <0.11 and >=0.11

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,137 +1,81 @@
 -- LSP Configuration: Language servers, Mason installer
 
 return {
-	-- LSP installer
+	-- LSP installer (install servers with :Mason)
+	-- Recommended servers: html, cssls, tailwindcss, clangd, rust_analyzer,
+	-- gopls, bashls, yamlls, taplo, zls, prismals, dockerls, jsonls, pyright, lua_ls
 	{
 		"williamboman/mason.nvim",
 		config = true,
 		build = ":MasonUpdate",
 	},
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup({
-				handlers = {
-					ts_ls = function() end,
-				},
-				ensure_installed = {
-					"html",
-					"cssls",
-					"tailwindcss",
-
-					"clangd",
-					"rust_analyzer",
-					"gopls",
-					"bashls",
-					"yamlls",
-					"taplo",
-					"zls",
-					"prismals",
-					"dockerls",
-					"jsonls",
-					"pyright",
-
-					"lua_ls",
-				},
-			})
-		end,
-	},
-
-	-- Native LSP support
+	-- Native LSP support (Neovim 0.11+ API)
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
+			"williamboman/mason.nvim",
 			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- Configure servers with filetypes (only start when needed)
-			vim.lsp.config("html", {
-				capabilities = capabilities,
-				filetypes = { "html" },
-			})
-			vim.lsp.config("cssls", {
-				capabilities = capabilities,
-				filetypes = { "css", "scss", "less" },
-			})
-			vim.lsp.config("tailwindcss", {
-				capabilities = capabilities,
-				filetypes = { "html", "css", "javascript", "javascriptreact", "typescript", "typescriptreact" },
-			})
-			vim.lsp.config("lua_ls", {
-				capabilities = capabilities,
-				filetypes = { "lua" },
-				settings = {
-					Lua = {
-						diagnostics = { globals = { "vim" } },
+			-- Neovim 0.11+ has native vim.lsp.config API, 0.10 uses lspconfig
+			if vim.fn.has("nvim-0.11") == 1 then
+				-- Modern API (0.11+)
+				vim.lsp.config("html", { capabilities = capabilities })
+				vim.lsp.config("cssls", { capabilities = capabilities })
+				vim.lsp.config("tailwindcss", { capabilities = capabilities })
+				vim.lsp.config("lua_ls", {
+					capabilities = capabilities,
+					settings = {
+						Lua = {
+							diagnostics = { globals = { "vim" } },
+						},
 					},
-				},
-			})
-			vim.lsp.config("clangd", {
-				capabilities = capabilities,
-				filetypes = { "c", "cpp", "objc", "objcpp" },
-			})
-			vim.lsp.config("rust_analyzer", {
-				capabilities = capabilities,
-				filetypes = { "rust" },
-			})
-			vim.lsp.config("gopls", {
-				capabilities = capabilities,
-				filetypes = { "go", "gomod", "gowork", "gotmpl" },
-			})
-			vim.lsp.config("bashls", {
-				capabilities = capabilities,
-				filetypes = { "sh", "bash" },
-			})
-			vim.lsp.config("yamlls", {
-				capabilities = capabilities,
-				filetypes = { "yaml", "yaml.docker-compose" },
-			})
-			vim.lsp.config("taplo", {
-				capabilities = capabilities,
-				filetypes = { "toml" },
-			})
-			vim.lsp.config("zls", {
-				capabilities = capabilities,
-				filetypes = { "zig" },
-			})
-			vim.lsp.config("prismals", {
-				capabilities = capabilities,
-				filetypes = { "prisma" },
-			})
-			vim.lsp.config("dockerls", {
-				capabilities = capabilities,
-				filetypes = { "dockerfile" },
-			})
-			vim.lsp.config("jsonls", {
-				capabilities = capabilities,
-				filetypes = { "json", "jsonc" },
-			})
-			vim.lsp.config("pyright", {
-				capabilities = capabilities,
-				filetypes = { "python" },
-			})
+				})
+				vim.lsp.config("clangd", { capabilities = capabilities })
+				vim.lsp.config("rust_analyzer", { capabilities = capabilities })
+				vim.lsp.config("gopls", { capabilities = capabilities })
+				vim.lsp.config("bashls", { capabilities = capabilities })
+				vim.lsp.config("yamlls", { capabilities = capabilities })
+				vim.lsp.config("taplo", { capabilities = capabilities })
+				vim.lsp.config("zls", { capabilities = capabilities })
+				vim.lsp.config("prismals", { capabilities = capabilities })
+				vim.lsp.config("dockerls", { capabilities = capabilities })
+				vim.lsp.config("jsonls", { capabilities = capabilities })
+				vim.lsp.config("pyright", { capabilities = capabilities })
 
-			-- Enable servers (will auto-start based on filetypes above)
-			vim.lsp.enable("html")
-			vim.lsp.enable("cssls")
-			vim.lsp.enable("tailwindcss")
-			vim.lsp.enable("lua_ls")
-			vim.lsp.enable("clangd")
-			vim.lsp.enable("rust_analyzer")
-			vim.lsp.enable("gopls")
-			vim.lsp.enable("bashls")
-			vim.lsp.enable("yamlls")
-			vim.lsp.enable("taplo")
-			vim.lsp.enable("zls")
-			vim.lsp.enable("prismals")
-			vim.lsp.enable("dockerls")
-			vim.lsp.enable("jsonls")
-			vim.lsp.enable("pyright")
+				-- Enable all configured servers
+				vim.lsp.enable("html", "cssls", "tailwindcss", "lua_ls", "clangd",
+					"rust_analyzer", "gopls", "bashls", "yamlls", "taplo", "zls",
+					"prismals", "dockerls", "jsonls", "pyright")
+			else
+				-- Legacy API (0.10 and earlier)
+				local lspconfig = require("lspconfig")
+				lspconfig.html.setup({ capabilities = capabilities })
+				lspconfig.cssls.setup({ capabilities = capabilities })
+				lspconfig.tailwindcss.setup({ capabilities = capabilities })
+				lspconfig.lua_ls.setup({
+					capabilities = capabilities,
+					settings = {
+						Lua = {
+							diagnostics = { globals = { "vim" } },
+						},
+					},
+				})
+				lspconfig.clangd.setup({ capabilities = capabilities })
+				lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+				lspconfig.gopls.setup({ capabilities = capabilities })
+				lspconfig.bashls.setup({ capabilities = capabilities })
+				lspconfig.yamlls.setup({ capabilities = capabilities })
+				lspconfig.taplo.setup({ capabilities = capabilities })
+				lspconfig.zls.setup({ capabilities = capabilities })
+				lspconfig.prismals.setup({ capabilities = capabilities })
+				lspconfig.dockerls.setup({ capabilities = capabilities })
+				lspconfig.jsonls.setup({ capabilities = capabilities })
+				lspconfig.pyright.setup({ capabilities = capabilities })
+			end
 		end,
 	},
 
