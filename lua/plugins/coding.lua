521b0bbfcf7254133d96471b07db55f07dbfb2c5
Max Ruman <max@mvsk.io>
2025-11-09 15:31:48 +0800

    add autoclose of jsx/tsx tags

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -61,6 +61,22 @@ return {
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 	},
 
+	-- Auto-close and auto-rename HTML/JSX/TSX tags
+	{
+		"windwp/nvim-ts-autotag",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require('nvim-ts-autotag').setup({
+				opts = {
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false -- Auto close on trailing </
+				}
+			})
+		end,
+	},
+
 	-- Code outline (modern tagbar)
 	{
 		"stevearc/aerial.nvim",
