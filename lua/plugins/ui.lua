9757fb2096ed916ee980754c4a7d703b1942ea69
Max Ruman <max@mvsk.io>
2025-11-24 03:24:15 +0800

    upgrade onedark to latest (1.0+)

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -152,7 +152,7 @@ return {
 	-- Theme
 	{
 		"navarasu/onedark.nvim",
-		version = "v0.1.0", -- Pin to v0.1.0 until comment highlighting is fixed in v1.0.0+
+		--version = "v0.1.0", -- Because [#251](https://github.com/navarasu/onedark.nvim/issues/251) was fixed
 		lazy = false,
 		priority = 1000,
 		config = function()
