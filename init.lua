a27b2b140f372bf935f27c4b8465fb913128cc34
Max Ruman <max@mvsk.io>
2025-11-24 16:41:36 +0800

    markdown code blocks are tabs

--- init.lua
+++ init.lua
@@ -132,16 +132,49 @@ vim.opt.smartindent = true
 vim.opt.preserveindent = true
 vim.opt.shiftround = true
 
--- Markdown-specific settings (4 spaces, expanded)
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
-	callback = function()
-		vim.opt_local.tabstop = 4
-		vim.opt_local.shiftwidth = 4
-		vim.opt_local.softtabstop = 4
-		vim.opt_local.expandtab = true
-	end,
-})
+-- Markdown code block indent: use tabs in fenced code blocks, spaces elsewhere
+vim.g.markdown_code_block_indent = true
+
+do
+	local function in_code_block()
+		local ok, node = pcall(vim.treesitter.get_node)
+		if not ok or not node then return false end
+		while node do
+			local t = node:type()
+			if t == "fenced_code_block" or t == "code_fence_content" then
+				return true
+			end
+			node = node:parent()
+		end
+		return false
+	end
+
+	local function update_md_indent()
+		if not vim.g.markdown_code_block_indent then return end
+
+		if in_code_block() then
+			vim.opt_local.expandtab = false
+			vim.opt_local.shiftwidth = 2
+		else
+			vim.opt_local.expandtab = true
+			vim.opt_local.shiftwidth = 4
+		end
+	end
+
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "markdown",
+		callback = function()
+			vim.opt_local.tabstop = 2
+			vim.opt_local.shiftwidth = 4
+			vim.opt_local.expandtab = true
+		end,
+	})
+
+	vim.api.nvim_create_autocmd("InsertEnter", {
+		pattern = "*.md",
+		callback = update_md_indent,
+	})
+end
 
 -- Completion
 vim.opt.completeopt = { "menuone", "noselect", "noinsert" }
